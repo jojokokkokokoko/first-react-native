cf9d6feaf28a55886e9558b5e303e991ba460f4b
Oleg <126164012+olreyplay@users.noreply.github.com>
2024-02-05 14:31:19 +0200

    1

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Counter from "./Counter";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Counter initialValue={0} />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightblue",
+  },
+});
