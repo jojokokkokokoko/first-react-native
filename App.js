250cda3597ead2507a0c6339a38580dac4339a3e
MyNameIs <noviceisart999@gmail.com>
2024-05-27 11:41:53 +0700

    first rect-native

--- App.js
+++ App.js
@@ -3,20 +3,20 @@ import { View, StyleSheet } from "react-native";
 import Counter from "./Counter";
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Counter initialValue={0} />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Counter initialValue={0} />
+		</View>
+	);
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "lightblue",
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "lightblue",
+	},
 });
